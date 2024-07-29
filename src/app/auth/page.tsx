776233d3550ec575c86b235dc507d656d551306e
Tamojit Das <tamojitdas181007@gmail.com>
2024-07-29 19:48:02 +0530

    app micro-opti : 2

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -12,23 +12,23 @@ const page = async () => {
     return (
         <div className="flex h-screen w-full">
 
-            <div className="flex-1 flex overflow-hidden dark:bg-[#651c2b55] bg-[#651c2b] relative justify-center items-center">
-                <img src="/redis-logo.svg" alt="redis logo" className="absolute -left-1/4 opacity-25 -bottom-52 lg:scale-125 xl:scale-100 scale-[2] pointer-events-none select-none -z-1" />
+            <div className="flex-1 flex overflow-hidden dark:bg-[#651c2b55] bg-[#651c2b55] relative justify-center items-center">
+                <img src="/bg.png" alt="app logo" className="absolute opacity-45 -bottom-4 -left-3 lg:scale-125 xl:scale-125 scale-[2] pointer-events-none select-none -z-1" />
 
                 <div className="flex flex-col gap-2 px-4 xl:ml-40 text-center md:text-start font-semibold">
                     <Image
                         src={"/logo.png"}
                         alt="logo"
-                         width={763}
-                        height={173}
+                        width={863}
+                        height={273}
                         className="mt-20 w-[420px] z-0 pointer-events-none select-none"
                     />
 
-                    <p className="text-2xl md:text-3xl text-balance z-10">
-                        The <span className="bg-red-500 px-2 font-bold text-white">ULTIMATE</span> Chat App
+                    <p className="text-2xl md:text-3xl text-balance z-10 ml-2.5">
+                        Tune into the <span className="bg-red-500 px-2 font-bold text-white">PERFECT VIBE</span>
                     </p>
-                    <p className="text-2xl md:text-3xl mb-32 text-balance z-10">
-                        You <span className="bg-green-500/90 font-bold px-2 text-white">NEED TO</span> build
+                    <p className="text-2xl md:text-3xl mb-32 text-balance z-10 ml-2.5">
+                        Stay <span className="bg-green-500/90 font-bold px-2 text-white">WILD</span> Stay <span className="bg-green-500/90 font-bold px-2 text-white">WEIRD</span> 
                     </p>
                     <AuthButtons />
                 </div>
@@ -36,7 +36,7 @@ const page = async () => {
 
             <div className="flex-1 relative overflow-hidden justify-center items-center hidden md:flex bg-noise">
                 <Image
-                    src={"/hero-right.png"}
+                    src={"/hero-png.png"}
                     alt="Hero Image"
                     fill
                     className="object-cover dark:opacity-60 opacity-90 pointer-events-none select-none h-full"
--- src/components/chat/ChatLayout.tsx
+++ src/components/chat/ChatLayout.tsx
@@ -6,6 +6,7 @@ import Sidebar from "../Sidebar";
 import MessageContainer from "./MessageContainer";
 import { User } from "@/db/dummy";
 import { useSelectedUser } from "@/store/useSelectedUser";
+import { useTheme } from "next-themes";
 
 //Typedef
 interface ChatLayoutProps {
@@ -17,6 +18,7 @@ const ChatLayout = ({ defaultLayout = [320, 480], users }: ChatLayoutProps) => {
     const [isMobile, setIsMobile] = useState(false);
     const [isCollapsed, setIsCollapsed] = useState(false);
     const { selectedUser } = useSelectedUser();
+    const { theme } = useTheme();
 
     useEffect(() => {
         const checkScreenWidth = () => {
@@ -68,7 +70,11 @@ const ChatLayout = ({ defaultLayout = [320, 480], users }: ChatLayoutProps) => {
                 {!selectedUser && (
                     <div className="flex justify-center items-center h-full w-full px-10">
                         <div className="flex flex-col justify-center items-center gp-4">
-                            <img src="/logo.png" alt='Logo' className="w-full md:w-2/3 lg:w-1/2" />
+                            {theme === "dark" ? (
+                                <img src="/logo.png" alt='Logo' className="w-full md:w-2/3 lg:w-1/2" />
+                            ) : (
+                                <img src="/logo-inverted.png" alt='Logo' className="w-full md:w-2/3 lg:w-1/2" />
+                            )}
                             <p className="text-muted-foreground text-center">Click on a chat to view the messages</p>
                         </div>
                     </div>
